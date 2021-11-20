be2c4f63a4213cb8905c01247d755c3b263e2cc5
chel1982 <chel@activist.com>
2021-11-20 15:23:24 +0200

    Убрал категорию "Без категории" из списка на странице Категории и остальное лишнее

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,13 +1,12 @@
 @extends('layout.site', ['title' => 'Каталог товаров'])
 
 @section('content')
-    <h1>Каталог товаров</h1>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ducimus, eligendi exercitationem expedita, iure iusto laborum magnam qui quidem repellat similique tempora tempore ullam! Deserunt doloremque impedit quis repudiandae voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ducimus, eligendi exercitationem expedita, iure iusto laborum magnam qui quidem repellat similique tempora tempore ullam! Deserunt doloremque impedit quis repudiandae voluptas.</p>
-
     <h2 class="mb-4">Разделы каталога</h2>
     <div class="row">
         @foreach ($roots as $root)
+            @if($root->id == 1)
+                @continue
+            @endif
             @include('catalog.part.category', ['category' => $root])
         @endforeach
     </div>
