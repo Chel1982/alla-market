8ba3f2d180363fb8e15900dc4054aea0cf31dd25
chel1982 <chel@activist.com>
2021-11-20 15:09:31 +0200

    Убрал категорию "Без категории" из списка на главной странице

--- resources/views/layout/part/branch.blade.php
+++ resources/views/layout/part/branch.blade.php
@@ -1,5 +1,8 @@
 <ul>
 @foreach ($items->where('parent_id', $parent) as $item)
+    @if($item->id == 1)
+        @continue
+    @endif
     <li>
         <a href="{{ route('catalog.category', ['category' => $item->slug]) }}">{{ $item->name }}</a>
         @if (count($items->where('parent_id', $item->id)))
