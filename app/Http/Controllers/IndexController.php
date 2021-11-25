a8dcfa01540d71fdc16400f74b780658c0c99399
chel1982 <chel@activist.com>
2021-11-25 15:46:00 +0200

    изменил вы вывод до 12 товаров

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller {
     public function __invoke(Request $request) {
-        $new = Product::whereNew(true)->latest()->limit(3)->get();
-        $hit = Product::whereHit(true)->latest()->limit(3)->get();
-        $sale = Product::whereSale(true)->latest()->limit(3)->get();
+        $new = Product::whereNew(true)->latest()->limit(12)->get();
+        $hit = Product::whereHit(true)->latest()->limit(12)->get();
+        $sale = Product::whereSale(true)->latest()->limit(12)->get();
         return view('index', compact('new', 'hit', 'sale'));
     }
 }
