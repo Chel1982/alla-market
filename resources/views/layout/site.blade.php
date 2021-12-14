1bf23efeec3e4b98007c2bc49f5d2ab771acce53
chel1982 <chel@activist.com>
2021-12-14 12:08:22 +0200

    Убрал просьбу к гууглу не индексировать сайт

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -6,7 +6,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="robots" content="noindex, nofollow"/>
     <title>{{ $title ?? 'Интернет-магазин' }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
